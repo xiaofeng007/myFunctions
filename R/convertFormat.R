2932cfd92a269518ef67c21b35c33976287362d2
xiaofeng007 <297946506@qq.com>
2019-11-14 10:28:04 +0800

    first commit

---
+++ R/convertFormat.R
@@ -0,0 +1,34 @@
+##' @title convert different formats from different softwares or packages
+##' @description This is an package to convert formats among different softwares or packages
+##'
+##' @details nothing
+##'
+##' @param seuObj the input seurat3 object
+##' @return an cell_data_set object
+##' @examples
+##' pbmc_raw <- read.table(file = system.file('extdata', 'pbmc_raw.txt', package = 'Seurat'),as.is = TRUE)
+##' pbmc_small <- CreateSeuratObject(counts = pbmc_raw)
+##' pbmc_monocle3 <- Seurat3ToMonocle3(pbmc_small)
+
+##'
+##' @author Feng Zhang
+
+Seurat3ToMonocle3 = function(seuObj){
+  cds <- new_cell_data_set(as(seuObj@assays$RNA@data, "sparseMatrix"),
+                           cell_metadata = as.data.frame(seuObj@meta.data),
+                           gene_metadata = data.frame(gene_short_name = rownames(seuObj), row.names = rownames(seuObj))
+  )
+  #assign cluster information from seurat object
+  recreate_partition <- factor(rep(1, length(cds@colData@rownames)))
+  names(recreate_partition) <- cds@colData@rownames
+  cds@clusters@listData[["UMAP"]][["partitions"]] <- recreate_partition
+
+  seurat_clusters=factor(seuObj@meta.data$seurat_clusters)
+  names(seurat_clusters) = row.names(seuObj@meta.data)
+  cds@clusters@listData[["UMAP"]][["clusters"]] = seurat_clusters
+
+  # assign reduced dimmension
+  cds@reducedDims@listData[["UMAP"]] <-seuObj@reductions[["umap"]]@cell.embeddings
+  cds@reducedDims@listData[["PCA"]] <-seuObj@reductions[["pca"]]@cell.embeddings
+  cds
+}
